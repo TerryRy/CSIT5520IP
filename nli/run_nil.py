a82bd1ce5274dc0cbcd31a8bb205d427585da314
TerryRy <2505293361@qq.com>
2026-05-08 11:58:09 +0800

    rename

--- nli/run_nil.py
+++ nli/run_nil.py
@@ -13,8 +13,8 @@ shots = [0, 1, 3]
 # =============================================
 
 # 加载课程提供的两个数据集
-matched = load_multinli_jsonl("matched_sampled.jsonl")      # ← 修改为你的实际文件名
-mismatched = load_multinli_jsonl("mismatched_sampled.jsonl") # ← 修改为你的实际文件名
+matched = load_multinli_jsonl("dev_matched_sampled-1.jsonl")      # ← 修改为你的实际文件名
+mismatched = load_multinli_jsonl("dev_mismatched_sampled-1.jsonl") # ← 修改为你的实际文件名
 
 def evaluate_dataset(df, model_key, shot=0):
     model, tokenizer = load_model_and_tokenizer(MODELS[model_key], 
